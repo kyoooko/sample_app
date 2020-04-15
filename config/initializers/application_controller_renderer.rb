c348246d6a6cd8894f5031b71bfa50e343369b49
飯田恭子 <iidakyouko@mba.local>
2020-04-15 18:54:56 +0900

    [Finish 3]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ff3b44b6999e3011339c39cb6d8d6bb4e5adf0843c34c75c4ca698a4302364d219c80f267ad2c13964f8766b2d8b88ae2c732b1797e182dd0d16235230e16944'
