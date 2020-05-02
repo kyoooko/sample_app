9db64bea910df481690dadb4bac03690a2369801
飯田恭子 <iidakyouko@mba.local>
2020-05-02 17:06:00 +0900

    [Finish 3]

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,7 +2,6 @@ class TodolistsController < ApplicationController
   def new
     @list = List.new
   end
-
   def create
       # ストロングパラメーターを使用
       list = List.new(list_params)
@@ -11,25 +10,20 @@ class TodolistsController < ApplicationController
       # 詳細画面へリダイレクト
       redirect_to todolist_path(list.id)
   end
-
   def index 
     @lists = List.all        
   end
-
   def show
     @list = List.find(params[:id]) 
   end
-
   def edit
     @list = List.find(params[:id])
   end
-
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
-
   def destroy
     list = List.find(params[:id]) 
     list.destroy 
